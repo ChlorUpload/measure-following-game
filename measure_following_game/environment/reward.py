e1fac60c585f072b79fd4a1b57d9a2f454ed2bab
kaparoo <kaparoo2001@gmail.com>
2022-05-18 00:48:22 +0900

    :sparkles: Add WeightedTriangleReward

--- measure_following_game/environment/reward.py
+++ measure_following_game/environment/reward.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import numpy as np
 from typing import ClassVar
 
 
@@ -20,4 +21,25 @@ class TriangleReward(RewardBase):
 
     def __call__(self, true_measure: int, pred_measure: int) -> float:
         abs_error = abs(true_measure - pred_measure)
+        abs_error = np.clip(abs_error, 0, self.threshold)
         return (self.threshold - abs_error) / self.threshold
+
+
+class WeightedTraingleReward(TriangleReward):
+    def __init__(
+        self,
+        window_size: int,
+        forward_weight: float = 1.0,
+        backward_weight: float = 1.0,
+    ):
+        super().__init__(window_size)
+        self.forward_weight = forward_weight
+        self.backward_weight = backward_weight
+
+    def __call__(self, true_measure: int, pred_measure: int) -> float:
+        reward = super().__call__(true_measure, pred_measure)
+        if true_measure <= pred_measure:
+            reward *= self.forward_weight
+        else:
+            reward *= self.backward_weight
+        return reward
